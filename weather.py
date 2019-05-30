6533c5541f77f08152c28d3c6ea6b0fb47b83dfd
loganballard <33067676+loganballard@users.noreply.github.com>
2019-05-29 22:58:17 -0700

    removing print

--- weather.py
+++ weather.py
@@ -11,5 +11,3 @@ def get_weather():
     res = requests.get('http://api.openweathermap.org/data/2.5/weather?zip={0},us&appid={1}&units=imperial'.format(ZIPCODE, API_KEY))
     weather = res.json()
     return weather.get('main')
-
-print(get_weather())
