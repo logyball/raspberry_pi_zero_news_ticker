e725b8e326e272565eea4c5ba0291f6a1e1cffe2
loganballard <33067676+loganballard@users.noreply.github.com>
2019-05-29 18:12:00 -0700

    added ability to get the top 3 google news headlines for the state and national news

---
+++ google_news_headlines.py
@@ -0,0 +1,31 @@
+# get the top 3 headlines split up by state and national news
+
+import feedparser, ssl
+
+STATE = 'Oregon'
+
+ssl._create_default_https_context=ssl._create_unverified_context
+
+def get_stories(url):
+    parsed_rss = feedparser.parse(url)
+    title_source_list = []
+    for story in parsed_rss['entries']:
+        title_source = story['title'].rsplit('-', 1)
+        title_source_list.append((title_source[0].rstrip(), title_source[1].lstrip()))
+        if len(title_source_list) > 2:
+            break
+    return title_source_list
+
+
+def get_national():
+    url = 'https://news.google.com/rss'
+    info = get_stories(url)
+    print(info)
+
+def get_local():
+    url = f'https://news.google.com/news/rss/headlines/section/geo/{STATE}'
+    info = get_stories(url)
+    print(info)
+
+get_national()
+get_local()
